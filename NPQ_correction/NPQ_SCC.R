1c797995e63db149910015017041b171163c3420
Kimberlee Baldry <baldry.kimberlee@gmail.com>
2025-07-27 09:23:33 +1000

    Add files via upload

--- NPQ_correction/NPQ_SCC.R
+++ NPQ_correction/NPQ_SCC.R
@@ -4,20 +4,23 @@ NPQ_SCC = function(depth, fluor, bbp, mld,fwl, sfm){
   # This NPQ correction is new, adjusted from Sackmann et al. (2008)
   #
   #
-  zeu_15 = Zeu(depth, fluor,0.15)
+  zeu_20 = Zeu(depth, fluor,0.2)
   if(is.na(fwl)){fwl = F}
-  if(fwl){int = NPQ_X12(depth,fluor,zeu_15)}else{
-    if(mld < sfm & mld < zeu_15){
-      if(is.na(bbp)){
+  if(fwl){int = NPQ_X12(depth,fluor,zeu_20)}else{
+    if(zeu_20 < sfm & mld < zeu_20){
+      if(all(is.na(bbp))){
         int = NPQ_X12(depth,fluor,mld)
       }else{int = NPQ_S08(depth,fluor,bbp,mld)}}
-    if(mld < sfm & mld > zeu_15){
-      if(is.na(bbp)){
-        int = NPQ_X12(depth,fluor,zeu_15)
-      }else{int = NPQ_S08(depth,fluor,bbp,zeu_15)}}
-    if(mld > sfm & mld < zeu_15){int = NPQ_X12(depth,fluor,mld)}
-    if(mld > sfm & mld > zeu_15){int = NPQ_X12(depth,fluor,zeu_15)}
+    if(zeu_20 < sfm & mld > zeu_20){
+      if(all(is.na(bbp))){
+        int = NPQ_X12(depth,fluor,zeu_20)
+      }else{int = NPQ_S08(depth,fluor,bbp,zeu_20)}}
+    if(zeu_20 > sfm & mld < zeu_20){int = NPQ_P18(depth,fluor,mld)}
+    if(zeu_20 > sfm & mld > zeu_20){
+      if(all(is.na(bbp))){
+        int = NPQ_X12(depth,fluor,zeu_20)
+      }else{int = NPQ_S08(depth,fluor,bbp,mld)}}
   }
-   return(list("NPQdepth" = int$NPQdepth, "corr_fluor" = int$corr_fluor))
+  return(list("NPQdepth" = int$NPQdepth, "corr_fluor" = int$corr_fluor))
 }
 
--- NPQ_correction/NPQ_XCC.R
+++ NPQ_correction/NPQ_XCC.R
@@ -2,14 +2,13 @@ NPQ_XCC = function(depth, fluor, bbp, mld, fwl,sfm){
   # This NPQ correction is new, adjusted from Xing et al. (2012)
   #
   #
-  zeu_15 = Zeu(depth, fluor,0.15)
+  zeu_20 = Zeu(depth, fluor,0.2)
   if(is.na(fwl)){fwl = F}
-  if(fwl){int = NPQ_X12(depth,fluor,zeu_15)}else{
-    if(mld < sfm & mld < zeu_15){int = NPQ_X12(depth,fluor,mld)}
-    if(mld < sfm & mld > zeu_15){int = NPQ_X12(depth,fluor,zeu_15)}
-    if(mld > sfm & mld < zeu_15){int = NPQ_X12(depth,fluor,mld)}
-    if(mld > sfm & mld > zeu_15){int = NPQ_X12(depth,fluor,zeu_15)}
+  if(fwl){int = NPQ_X12(depth,fluor,zeu_20)}else{
+    if(zeu_20 < sfm & mld < zeu_20){int = NPQ_X12(depth,fluor,mld)}
+    if(zeu_20 < sfm & mld > zeu_20){int = NPQ_X12(depth,fluor,zeu_20)}
+    if(zeu_20 > sfm & mld < zeu_20){int = NPQ_P18(depth,fluor,mld)}
+    if(zeu_20 > sfm & mld > zeu_20){int = NPQ_X12(depth,fluor,zeu_20)}
   }
   return(list("NPQdepth" = int$NPQdepth, "corr_fluor" = int$corr_fluor))
 }
-
